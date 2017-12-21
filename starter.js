2de5fa270044a4bc5d2207d980b94966682f004d
Protimaroy <32514552+Protimaroy@users.noreply.github.com>
2017-12-21 17:36:28 -0500

    contacts
    manage

---
+++ starter.js
@@ -0,0 +1,5 @@
+let contactStarter = [
+	{"fullname":"Kevin Costner","phone":"613-400-0091","email":"kevin_costner@bodyguard.com"},
+	{"fullname":"Richard Gere","phone":"613-770-9491","email":"richard.g@runawaybride.org"},
+	{"fullname":"Tom Cruise","phone":"613-399-4456","email":"tom_cruise@cocktail.com"}
+];
